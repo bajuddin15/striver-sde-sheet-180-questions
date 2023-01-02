bc0a48ff6222bd4b6348d86bdec5c94ba805fdc9
Bajuddin Khan <bajuddinkhan786@gmail.com>
2023-01-03 00:02:52 +0530

    solve reverse linked list problem

---
+++ striver-sde-sheet/Linked List/reverseLinkedList.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prevptr = NULL;
+        ListNode* currptr = head;
+        ListNode* nextptr ;
+        while(currptr != NULL){
+            nextptr = currptr->next;
+            currptr->next = prevptr;
+            prevptr = currptr;
+            currptr = nextptr;
+        }
+        return prevptr;
+        
+        
+    }
+};
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
